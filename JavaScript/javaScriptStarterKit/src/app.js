adece372810c58a8d5f449e229c8fbf320df85b8
Nuri Buğra Demir <bugranuri.demir@gmail.com>
2023-05-12 04:31:12 +0300

    fix

--- JavaScript/javaScriptStarterKit/src/app.js
+++ JavaScript/javaScriptStarterKit/src/app.js
@@ -1,25 +1,25 @@
-console.log("Hello World!")
+console.log("Hello World!");
 
-let dolarDun = 19.90
-let dolarBugun = 20.01
+let dolarDun = 19.90;
+let dolarBugun = 20.01;
 // var keyword'ü artık kullanılmıyor. Onun yerine let kullan.
 
 {
-    let dolarDun = 19.80
+    let dolarDun = 19.80;
 }
 
-console.log(dolarDun)
+console.log(dolarDun);
 
 // array
 // camelCasing
 // PascalCasing
-let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
 
 console.log("<ul>")
 for (let i = 0; i < konutKredileri.length; i++) {
-    console.log("<li>" + konutKredileri[i] + "</li>")
+    console.log("<li>" + konutKredileri[i] + "</li>");
 }
 console.log("</ul>")
 
 
-console.log(konutKredileri)
+console.log(konutKredileri);
